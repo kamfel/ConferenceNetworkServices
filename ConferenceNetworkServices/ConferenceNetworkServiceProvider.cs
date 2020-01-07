3438a05c1ed24286727ecba756aaa052dad78c4b
kamfel <kamil.fel76@gmail.com>
2020-01-07 17:58:33 +0100

    Add services and service provider

---
+++ ConferenceNetworkServices/ConferenceNetworkServiceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConferenceNetworkServices
+{
+    public class ConferenceNetworkServiceProvider : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

23bc1dfdfa5a9273a551d3e8c8fb3fdf642b2f99
kamfel <kamil.fel76@gmail.com>
2020-01-29 19:16:49 +0100

    Add phone number to users

--- ConferenceNetworkServices/DTO/UserInfoDTO.cs
+++ ConferenceNetworkServices/DTO/UserInfoDTO.cs
@@ -10,5 +10,6 @@ namespace ConferenceNetworkServices.DTO
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
--- ConferenceNetworkServices/DTO/UserRegisterDTO.cs
+++ ConferenceNetworkServices/DTO/UserRegisterDTO.cs
@@ -13,5 +13,7 @@ namespace ConferenceNetworkServices.DTO
         public string Email { get; set; }
 
         public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
